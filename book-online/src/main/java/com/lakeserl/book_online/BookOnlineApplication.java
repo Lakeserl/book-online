ce2ecb708b93533b9f85a99ab7f42e62c2987063
Lakeserl <dinhlam18112003@gmail.com>
2025-03-27 00:12:04 +0700

    Create

---
+++ book-online/src/main/java/com/lakeserl/book_online/BookOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.lakeserl.book_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOnlineApplication.class, args);
+	}
+
+}
---
+++ book-online/src/test/java/com/lakeserl/book_online/BookOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakeserl.book_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
